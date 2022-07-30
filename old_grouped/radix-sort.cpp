51f058be2092f4288055a81dc597047031563388
Tiago Ceccon <ceccon.t@hotmail.com>
2022-07-30 15:29:43 -0300

    Isolate Radixsort code into self-contained folder

---
+++ old_grouped/radix-sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+
+void copy(int *dest, int *src, int size) {
+   for (int i=0; i < size; i++)
+       dest[i] = src[i];
+}
+
+void radix_sort_LSD(int C[], int tam){
+    int aux[tam];
+    int dig = 1, higher = C[0];
+
+    for (int i = 1; i < tam; i++) 
+        if (C[i] > higher) 
+            higher = C[i];
+
+    while (higher / dig > 0){
+        int acm[10] = {0};
+        for (int i = 0; i < tam; i++) acm[C[i] / dig % 10]++;
+        for (int i = 1; i < 10; i++) acm[i] += acm[i - 1];
+        for (int i = tam - 1; i >= 0; i--) aux[--acm[C[i] / dig % 10]] = C[i];
+
+        copy(C, aux, tam);
+        dig *= 10;
+    }
+}
--- radixsort/main.c
+++ radixsort/main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "crc32.c"
+#include "radix-sort.c"
 
 int main (int argc, char *argv[]){
     int size;
@@ -10,16 +10,16 @@ int main (int argc, char *argv[]){
     size = 3000;
 
     int v[size];
-    unsigned long crc_value = 1;
 
     for (int i = 0; i < size; i++) {
         v[i] = random() % 100000;
     }
 
     // Run application
-    for (int i = 1; i < size; i++) {
-        crc_value = Crc32_ComputeBuf(crc_value, &v[i], sizeof(int));
-    }
+    radix_sort_LSD(v, size);
+
+    // If debug is needed
+    // for (int i = 1; i < size; i++) if (v[i-1] > v[i]) std::cout << "Wrong order!\n";
 
     return 0;
 
