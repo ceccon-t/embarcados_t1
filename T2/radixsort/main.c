f940522fad0cc6b061467e7b7a5cf14e0a94a530
Tiago Ceccon <ceccon.t@hotmail.com>
2022-09-07 22:06:19 -0300

    Adiciona codigo do radixsort alterado para medir energia

---
+++ T2/radixsort/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "radix-sort.c"
+#include "energia.h"
+
+int main (int argc, char *argv[]){
+    int size;
+    srand(177542);
+
+    size = 3000;
+
+    int v[size];
+
+    for (int i = 0; i < size; i++) {
+        v[i] = random() % 100000;
+    }
+
+    // Run application
+    rapl_init();  /* inicializar a lib do RAPL */
+	start_rapl_sysfs(); // Iniciar a contagem de consumo de energia
+    clock_t t = clock(); // Iniciar a contagem de tempo
+
+        radix_sort_LSD(v, size); // O que sera medido
+
+    t = clock() - t; // Finalizar contagem do tempo
+    double energy = end_rapl_sysfs();   // Finalizar a contagem dos contadores de energia
+    double tempo = ((double)t)/CLOCKS_PER_SEC; // transforma tempo para segundos
+    printf("Tempo de execucao em segundos: %.5f\n", tempo);
+    printf("Energia consumida em Joules:   %.5f\n", energy); // (6) imprimir consumo de energia em Joules
+
+    // If debug is needed
+    // for (int i = 1; i < size; i++) if (v[i-1] > v[i]) std::cout << "Wrong order!\n";
+
+    return 0;
+
+}
